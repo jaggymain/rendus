91f283dfde7220c637c4a47bad888f0248f1bbb7
Nick Wallace <nickw@nicks-mbp.lan>
2025-12-16 18:49:50 +0000

    Update stripe config

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -9,7 +9,7 @@ function getStripeInstance(): Stripe | null {
   }
   
   return new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2024-12-18.acacia',
+    apiVersion: '2025-11-17.clover',
     typescript: true,
   })
 }
